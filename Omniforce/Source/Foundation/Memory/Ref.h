5da2e2791c54b354363abfa5cbea225abe279915
Daniil Vinnik <daniil.vinnik1410@gmail.com>
2025-01-11 21:53:49 +0100

    refactor: now always make one allocation for Ref
    - removed the ability to split object and counter allocations, now always allocate within one memory block

--- Omniforce/Source/Foundation/Memory/Ref.h
+++ Omniforce/Source/Foundation/Memory/Ref.h
@@ -5,7 +5,6 @@
 
 #include <Foundation/Memory/MemoryAllocation.h>
 #include <Foundation/Memory/Allocator.h>
-#include <Foundation/Memory/WeakPtr.h>
 #include <Foundation/Assert.h>
 
 namespace Omni {
@@ -15,11 +14,11 @@ namespace Omni {
 	template<typename T>
 	class OMNIFORCE_API Ref {
 	public:
-		using CounterType = uint64;
+		using TCounter = uint64;
 
 		struct StorageType {
 			template<typename... TArgs>
-				requires (!std::is_abstract_v<T>)
+			requires (!std::is_abstract_v<T>)
 			StorageType(TArgs&&... args)
 				: object({}), ref_counter(1)
 			{
@@ -27,14 +26,20 @@ namespace Omni {
 			}
 
 			template<typename... TArgs>
-				requires std::is_abstract_v<T>
+			requires std::is_abstract_v<T>
 			StorageType(TArgs&&... args)
 				: object({}), ref_counter(1)
 			{
 				OMNIFORCE_ASSERT_TAGGED(false, "Cannot instantiate abstract class");
 			}
 
-			Atomic<CounterType> ref_counter;
+			~StorageType()
+			{
+				T* typed_object;
+				typed_object->~T();
+			}
+
+			Atomic<TCounter> ref_counter;
 			byte object[sizeof(T)]; // a hack that forbids allocation of T explicitly
 		};
 
@@ -42,39 +47,18 @@ namespace Omni {
 		Ref(IAllocator* allocator, TArgs&&... args)
 			: m_Allocator(allocator)
 		{
-			// Split allocations if size will be different if we don't split them.
-			// In this case, it is likely that persistent allocator is used and it will be able to make additional splits
-			// that may be used for other counters as well
-			if (m_Allocator->ComputeAlignedSize(sizeof(StorageType)) != m_Allocator->ComputeAlignedSize(sizeof(T))
-				+ m_Allocator->ComputeAlignedSize(sizeof(CounterType)))
-			{
-				m_Allocation = m_Allocator->Allocate<T>(std::forward<TArgs>(args)...);
-				m_CounterAllocation = m_Allocator->Allocate<CounterType>(1);
+			// Make one allocation for both counter and object so we get better caching
+			m_Allocation = m_Allocator->Allocate<StorageType>(std::forward<TArgs>(args)...);
 
-				// Cache pointers
-				m_CachedObject = m_Allocation.As<T>();
-				m_CachedCounter = m_CounterAllocation.As<Atomic<CounterType>>();
-
-			}
-			// Otherwise, make one allocation for both counter and object if size will be the same 
-			// (it is likely that transient or dedicated allocators are used)
-			else {
-				m_Allocation = m_Allocator->Allocate<StorageType>(std::forward<TArgs>(args)...);
-				m_CounterAllocation = m_Allocation;
-
-				// Cache object and counter pointers so we don't need to add `sizeof(CounterType)` each time we access an object
-				m_CachedObject = (T*)(m_Allocation.Memory + sizeof(CounterType));
-				m_CachedCounter = m_Allocation.As<Atomic<CounterType>>();
-			}
+			// Cache object and pointer so we don't need to add `sizeof(CounterType)` each time we access an object
+			m_CachedObject = (T*)(m_Allocation.Memory + sizeof(TCounter));
 		};
 
 	public:
 		Ref()
 			: m_Allocator(nullptr)
 			, m_Allocation(MemoryAllocation::InvalidAllocation())
-			, m_CounterAllocation(MemoryAllocation::InvalidAllocation())
 			, m_CachedObject(nullptr)
-			, m_CachedCounter(nullptr)
 		{
 		}
 
@@ -82,11 +66,8 @@ namespace Omni {
 		Ref(std::nullptr_t)
 			: m_Allocator(nullptr)
 			, m_Allocation(MemoryAllocation::InvalidAllocation())
-			, m_CounterAllocation(MemoryAllocation::InvalidAllocation())
 			, m_CachedObject(nullptr)
-			, m_CachedCounter(nullptr)
-		{
-		}
+		{}
 
 		// Delete all other variations except nullptr
 		template<typename U>
@@ -105,9 +86,7 @@ namespace Omni {
 		Ref(const Ref& other)
 			: m_Allocator(other.m_Allocator)
 			, m_Allocation(other.m_Allocation)
-			, m_CounterAllocation(other.m_CounterAllocation)
 			, m_CachedObject((T*)other.m_CachedObject)
-			, m_CachedCounter(other.m_CachedCounter)
 		{
 			IncrementRefCounter();
 		};
@@ -116,9 +95,7 @@ namespace Omni {
 		Ref(const Ref<U>& other)
 			: m_Allocator(other.m_Allocator)
 			, m_Allocation(other.m_Allocation)
-			, m_CounterAllocation(other.m_CounterAllocation)
 			, m_CachedObject((T*)other.m_CachedObject)
-			, m_CachedCounter(other.m_CachedCounter)
 		{
 			static_assert(std::is_same_v<T, U> || std::is_base_of_v<T, U> || std::is_base_of_v<U, T>, "Types are not related to each other");
 
@@ -129,9 +106,7 @@ namespace Omni {
 		Ref(Ref<U>&& other) noexcept
 			: m_Allocator(other.m_Allocator)
 			, m_Allocation(other.m_Allocation)
-			, m_CounterAllocation(other.m_CounterAllocation)
 			, m_CachedObject(other.m_CachedObject)
-			, m_CachedCounter(other.m_CachedCounter)
 		{
 			static_assert(std::is_same_v<T, U> || std::is_base_of_v<T, U>, "Types are not related to each other");
 
@@ -155,15 +130,12 @@ namespace Omni {
 
 			return WeakPtr<U>(allocation);
 		}
-
 		void Reset() {
 			DecrementRefCounter();
 
 			m_Allocator = nullptr;
 			m_Allocation = MemoryAllocation::InvalidAllocation();
-			m_CounterAllocation = MemoryAllocation::InvalidAllocation();
 			m_CachedObject = nullptr;
-			m_CachedCounter = nullptr;
 
 			if (CanReleaseAllocation()) {
 				ReleaseAllocation();
@@ -186,9 +158,7 @@ namespace Omni {
 
 			m_Allocator = other.m_Allocator;
 			m_Allocation = other.m_Allocation;
-			m_CounterAllocation = other.m_CounterAllocation;
 			m_CachedObject = (T*)other.m_CachedObject;
-			m_CachedCounter = other.m_CachedCounter;
 
 			IncrementRefCounter();
 
@@ -214,9 +184,7 @@ namespace Omni {
 
 			m_Allocator = other.m_Allocator;
 			m_Allocation = other.m_Allocation;
-			m_CounterAllocation = other.m_CounterAllocation;
 			m_CachedObject = (T*)other.m_CachedObject;
-			m_CachedCounter = other.m_CachedCounter;
 
 			IncrementRefCounter();
 
@@ -239,9 +207,7 @@ namespace Omni {
 
 			m_Allocator = other.m_Allocator;
 			m_Allocation = other.m_Allocation;
-			m_CounterAllocation = other.m_CounterAllocation;
 			m_CachedObject = other.m_CachedObject;
-			m_CachedCounter = other.m_CachedCounter;
 
 			IncrementRefCounter();
 
@@ -269,9 +235,7 @@ namespace Omni {
 
 			m_Allocator = other.m_Allocator;
 			m_Allocation = other.m_Allocation;
-			m_CounterAllocation = other.m_CounterAllocation;
 			m_CachedObject = other.m_CachedObject;
-			m_CachedCounter = other.m_CachedCounter;
 
 			IncrementRefCounter();
 
@@ -305,7 +269,7 @@ namespace Omni {
 				return false;
 			}
 
-			if (*m_CachedCounter != 0) {
+			if (GetCounter() != 0) {
 				return false;
 			}
 
@@ -314,38 +278,29 @@ namespace Omni {
 
 		inline void ReleaseAllocation() {
 			// Free resource
-			// Check if counter and object allocation were split
-			if (m_CounterAllocation == m_Allocation) {
-				m_Allocator->Free<StorageType>(m_Allocation);
-			}
-			// Otherwise clear both allocations
-			else {
-				m_Allocator->Free<T>(m_Allocation);
-				m_Allocator->Free<CounterType>(m_Allocation);
-			}
+			m_Allocator->Free<StorageType>(m_Allocation);
 		}
 
 		inline void IncrementRefCounter() {
 			if (m_Allocation.IsValid()) {
-				(*m_CachedCounter)++;
+				GetCounter()++;
 			}
 		}
 
 		inline void DecrementRefCounter() {
 			if (m_Allocation.IsValid()) {
-				(*m_CachedCounter)--;
+				GetCounter()--;
 			}
+		}
+
+		inline Atomic<TCounter>& GetCounter() {
+			return *(m_Allocation.As<Atomic<TCounter>>());
 		}
 
 	private:
 		IAllocator* m_Allocator;
-
 		MemoryAllocation m_Allocation;
-		MemoryAllocation m_CounterAllocation;
-
 		T* m_CachedObject;
-		Atomic<CounterType>* m_CachedCounter;
-
 	};
 
 	template<typename T, typename... TArgs>
