e1f64bb6d6d53e129b23bef871a2cd10c16694d4
Daniil Vinnik <daniil.vinnik1410@gmail.com>
2025-06-06 13:22:18 +0200

    Revert "fix: deleted Runtime resources dir"
    This reverts commit 87d992c08354cc44c1cce54ea007af4ac1278d8d.

---
+++ Runtime/Resources/SandboxProject/assets/scripts/src/Script.cs
@@ -0,0 +1,25 @@
+﻿using Omni; // using engine namespace
+
+// Use whatever namespace you want
+namespace test
+{
+    // Must inherit from Omni.GameObject to create script
+    public class MyScript : GameObject
+    {
+        // Must implement OnInit()
+        // Represents constructor and called before first frame starts
+        public override void OnInit()
+        {
+            Debug.Log(MessageSeverity.TRACE, "test");
+        }
+
+        // Must implement OnUpdate(float ts), where ts is time step
+        // Called every frame
+        public override void OnUpdate()
+        {
+            if(Input.KeyPressed(KeyCode.KEY_W)) {
+                Debug.Log(MessageSeverity.INFO, "Test internal call");
+            }
+        }
+    }  
+}
---
+++ Runtime/Resources/Scripts/ScriptsProject/Scripts/Source/Script.cs
@@ -0,0 +1,23 @@
+﻿using Omni; // using engine namespace
+
+// Use whatever namespace you want
+namespace Game
+{
+    // Must inherit from Omni.GameObject to create script
+    public class MyScript : GameObject
+    {
+        // Must implement OnInit()
+        // Represents constructor and called before first frame starts
+        public override void OnInit()
+        {
+            
+        }
+
+        // Must implement OnUpdate(), where ts is time step
+        // Called every frame
+        public override void OnUpdate()
+        {
+            
+        }
+    }  
+}
